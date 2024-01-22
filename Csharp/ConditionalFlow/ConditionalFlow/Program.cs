1b9ba1e08d1e1780f6791854332746bf8dec7c29
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-22 14:06:10 +0300

    if vs switch-case

---
+++ Csharp/ConditionalFlow/ConditionalFlow/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("şu seçeneklerden birini girin: kirmizi, sari, yesil");
+string trafikLambasi = Console.ReadLine();
+
+if (trafikLambasi == "kirmizi")
+{
+    Console.WriteLine("dur");
+}
+else if (trafikLambasi == "sari")
+{
+    Console.WriteLine("bekle");
+
+}
+else if (trafikLambasi == "yesil")
+{
+    Console.WriteLine("geç");
+
+}
+else
+{
+    Console.WriteLine("hatalı giriş");
+}
+
+
+switch (trafikLambasi)
+{
+    case "kirmizi":
+        Console.WriteLine("dur");
+
+        break;
+    case "sari":
+        Console.WriteLine("bekle");
+
+        break;
+    case "yesil":
+        Console.WriteLine("geç!");
+
+        break;
+    default:
+        break;
+}
--- Csharp/VariableAndTypes/VariableAndTypes/Program.cs
+++ Csharp/VariableAndTypes/VariableAndTypes/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
 Console.WriteLine("........ Değişkenler ............");
 //Primitive type: Sadece birim değer için oluşturulmuş tipler.
 
@@ -31,4 +33,57 @@ decimal decimalPi = 3.145946789610666514444044646404945411548962033266564M;
 //Sözel:
 char character = '{';
 string companyName = "Halkbank";
+
+//Point point = new Point();
+//Point point2 = new Point();
+//Mantıksal veri tipi:
+bool isSuccess = true;
+
+byte number = 100;
+int implicitCasting = number;
+
+int minimum = 510;
+byte explicitCasting = (byte)minimum;
+
+Console.WriteLine(explicitCasting);
+
+try
+{
+    Console.WriteLine("Bir sayı giriniz:");
+    var number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Bir sayı daha giriniz:");
+    var number2 = int.Parse(Console.ReadLine());
+    checked
+    {
+        byte total = (byte)(number1 + number2);
+        Console.WriteLine($"Toplam: {total} ");
+
+        byte division = (byte)(number1 / number2);
+        Console.WriteLine($"Bölüm sonucu: {division}");
+
+    }
+
+}
+catch (FormatException)
+{
+
+    Console.WriteLine("Sayı girilmediği için hata oluştu");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Girdiğiniz değerler toplamı, byte sınırlarında olmalı");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("0'a bölünemez");
+}
+
+
+
+
+
+
+
+
+
 
