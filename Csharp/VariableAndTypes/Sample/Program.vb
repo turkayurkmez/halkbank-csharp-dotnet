5bbd6f61e0b0fa2e07f42aba3f4c210f572b8c67
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-22 11:53:06 +0300

    initialize repo

---
+++ Csharp/VariableAndTypes/Sample/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim number As Integer
+
+        Dim number2 As Int32
+
+    End Sub
+End Module
---
+++ Csharp/VariableAndTypes/VariableAndTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("........ Değişkenler ............");
+//Primitive type: Sadece birim değer için oluşturulmuş tipler.
+
+/*
+ *  Sayısal
+ *    Tam Sayılar *    
+ *    Ondalıklı sayılar
+ *  
+ *  Sözel
+ *     
+ *  
+ *  Mantıksal
+ *  
+ */
+
+byte miniInteger = 255;
+sbyte possiblyNegative = -127;
+
+short sixteenBit = 32767;
+ushort unsignedShort = 65535;
+
+
+//int, uint, long, ulong
+
+//Ondalıklı
+double pi = 3.14;
+float floatPi = 3.14f;
+decimal decimalPi = 3.145946789610666514444044646404945411548962033266564M;
+
+//Sözel:
+char character = '{';
+string companyName = "Halkbank";
+
