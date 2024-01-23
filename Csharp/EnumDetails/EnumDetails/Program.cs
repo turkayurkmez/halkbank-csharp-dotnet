5fa7ca8d88c37da39cbe51f263ec9aa382347688
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-23 11:12:15 +0300

    Enum details (spesifically flags)

---
+++ Csharp/EnumDetails/EnumDetails/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EnumDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            AkrabalikTipleri tip = AkrabalikTipleri.Abi |
+                                   AkrabalikTipleri.Ogul |
+                                   AkrabalikTipleri.Baba |
+                                   AkrabalikTipleri.Amca |
+                                   AkrabalikTipleri.Dayi |
+                                   AkrabalikTipleri.Dede;
+
+            Console.WriteLine((int)(tip));
+        }
+    }
+
+    public enum AkrabalikTipleri
+    {
+        Abi = 1,
+        Ogul = 2,
+        Baba = 4,
+        Amca = 8,
+        Dayi = 16,
+        Dede = 32
+    }
+}
