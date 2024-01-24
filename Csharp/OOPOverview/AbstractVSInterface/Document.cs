30377e0c65c6ae5c96f7e379abab2780600f6bd5
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-24 10:35:53 +0300

    Abstract and Interface

---
+++ Csharp/OOPOverview/AbstractVSInterface/Document.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractVSInterface
+{
+    public abstract class Document
+    {
+        public string Title { get; set; }
+        public string Creator { get; set; }
+        public void Copy(string from, string to)
+        {
+            Console.WriteLine("kopyalandı");
+        }
+
+        public void Move(string from, string to)
+        {
+            Console.WriteLine("taşındı");
+        }
+
+        public abstract void Save();
+        public abstract void Load();
+        public abstract void Print();
+
+
+    }
+
+    public class WordDocument : Document
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Word çıktısı alındı");
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExcelDocument : Document
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Excel çıktısı alındı");
+
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PdfDocument : Document
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("PDF çıktısı alındı");
+
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class PrinterComponent
+    {
+        public void Print(Document document)
+        {
+            document.Print();
+        }
+    }
+}
--- Csharp/OOPOverview/AbstractVSInterface/Program.cs
+++ Csharp/OOPOverview/AbstractVSInterface/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AbstractVSInterface;
+
 Console.WriteLine("Hello, World!");
+WordDocument word = new WordDocument();
+ExcelDocument excel = new ExcelDocument();
+PdfDocument pdf = new PdfDocument();
+
+PrinterComponent printer = new PrinterComponent();
+printer.Print(word);
+printer.Print(excel);
+printer.Print(pdf);
