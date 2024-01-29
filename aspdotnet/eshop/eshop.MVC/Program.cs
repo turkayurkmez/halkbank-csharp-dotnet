ab312661e23dc8c6d8a58755f75cffdc1b291c3c
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-29 16:05:34 +0300

    Program.cs  saved

--- aspdotnet/eshop/eshop.MVC/Program.cs
+++ aspdotnet/eshop/eshop.MVC/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IProductRepository, FakeProductRepository>();
 
