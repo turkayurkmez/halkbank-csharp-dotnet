57f8eb754593ef0c3db7df4232f4fe20d0cb3a74
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-30 10:39:48 +0300

    DI Lifetime POC completed

---
+++ aspdotnet/DILifeTime/DILifeTime/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using DILifeTime.Models;
+using DILifeTime.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DILifeTime.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ISingletonGuid singleton;
+        private readonly ITransientGuid transient;
+        private readonly IScopedGuid scoped;
+        private readonly GuidService guidService;
+
+        public HomeController(ILogger<HomeController> logger, ISingletonGuid singleton, ITransientGuid transient, IScopedGuid scoped, GuidService guidService)
+        {
+            _logger = logger;
+            this.singleton = singleton;
+            this.transient = transient;
+            this.scoped = scoped;
+            this.guidService = guidService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SingletonValue = singleton.Guid.ToString();
+            ViewBag.TransientValue = transient.Guid.ToString();
+            ViewBag.ScopedValue = scoped.Guid.ToString();
+
+            ViewBag.SingletonScope = guidService.SingletonGuid.Guid.ToString();
+            ViewBag.TransientScope = guidService.TransientGuid.Guid.ToString();
+            ViewBag.ScopedScope = guidService.ScopedGuid.Guid.ToString();
+
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ aspdotnet/DILifeTime/DILifeTime/Services/IGuidGenerator.cs
@@ -0,0 +1,61 @@
+﻿namespace DILifeTime.Services
+{
+    public interface IGuidGenerator
+    {
+        Guid Guid { get; }
+    }
+
+    //Üç yöntem var: Singleton, Transient ve Scoped
+    public interface ISingletonGuid : IGuidGenerator { }
+    public interface ITransientGuid : IGuidGenerator { }
+    public interface IScopedGuid : IGuidGenerator { }
+
+    public class Singleton : ISingletonGuid
+    {
+        public Singleton()
+        {
+            Guid = Guid.NewGuid();
+        }
+        public Guid Guid { get; }
+    }
+
+    public class Scoped : IScopedGuid
+    {
+        public Scoped()
+        {
+            Guid = Guid.NewGuid();
+
+        }
+        public Guid Guid { get; }
+    }
+
+    public class Transient : ITransientGuid
+    {
+        public Transient()
+        {
+            Guid = Guid.NewGuid();
+
+        }
+        public Guid Guid { get; }
+    }
+
+
+    public class GuidService
+    {
+
+        public ISingletonGuid SingletonGuid { get; }
+        public IScopedGuid ScopedGuid { get; }
+        public ITransientGuid TransientGuid { get; }
+
+        public GuidService(ISingletonGuid singletonGuid, IScopedGuid scopedGuid, ITransientGuid transientGuid)
+        {
+
+            SingletonGuid = singletonGuid;
+            ScopedGuid = scopedGuid;
+            TransientGuid = transientGuid;
+        }
+
+
+    }
+
+}
